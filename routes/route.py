67170e710bdc2fc3384713769dac19644535bbab
Vinay Phatarpekar <vinayp9399@gmail.com>
2025-12-15 12:41:10 +0530

    done

--- routes/route.py
+++ routes/route.py
@@ -9,7 +9,7 @@ router = APIRouter()
 @router.get("/")
 async def get_todos():
     todos = list_serial(collection_name.find())
-    return todos
+    return {'name':'Vinay'}
 
 @router.post("/")
 async def post_todo(todo:Todo):
