2c15649261bd91ce99e57c9ae30e41c679937162
Vinay Phatarpekar <vinayp9399@gmail.com>
2025-12-15 20:02:20 +0530

    done

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
+        # "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://e-comwebsite-frontend.vercel.app"
     ],
